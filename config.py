bd25f09a2ed1725fe33df75e157022e7f15973a5
Hieronym <cicero225@gmail.com>
2025-04-26 17:35:17 -0700

    Clairfy what the model names are

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
+from typing import Literal
+
 # Original configuration for the application. Unfortunately I am not very disciplined so only a small amount of settings are here.
 ANTHROPIC_MODEL_NAME = "claude-3-7-sonnet-20250219"
 GEMINI_MODEL_NAME = "gemini-2.5-pro-preview-03-25"  # Or, for example, "gemini-2.5-flash-preview-04-17"
 OPENAI_MODEL_NAME = "o3"  # o4-mini also works.
 
 # This configures what family of models we end up using.
-MODEL = "OPENAI"
+MODEL: Literal["CLAUDE", "GEMINI", "OPENAI"] = "CLAUDE"
 
 # Currently only for the unused navigation_assistance feature. Can be ignored.
-MAPPING_MODEL = "OPENAI"
+MAPPING_MODEL: Literal["CLAUDE", "GEMINI", "OPENAI"] = "CLAUDE"
 
 
 TEMPERATURE = 1.0
