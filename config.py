ae18d33fe601dbf2d76358c1150d7f5c1d67466c
Hieronym <cicero225@gmail.com>
2025-05-31 15:45:45 -0700

    Updating models to newer models

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 from typing import Literal
 
 # Original configuration for the application. Unfortunately I am not very disciplined so only a small amount of settings are here.
-ANTHROPIC_MODEL_NAME = "claude-3-7-sonnet-20250219"
+ANTHROPIC_MODEL_NAME = "claude-opus-4-20250514"
 GEMINI_MODEL_NAME = "gemini-2.5-pro-preview-03-25"  # Or, for example, "gemini-2.5-flash-preview-04-17"
 OPENAI_MODEL_NAME = "o3"  # o4-mini also works.
 
 # This configures what family of models we end up using.
 MODEL: Literal["CLAUDE", "GEMINI", "OPENAI"] = "CLAUDE"
 # Model used for delegated tasks.
-ANTHROPIC_MINI_MODEL_NAME = "claude-3-7-sonnet-20250219" #"claude-3-5-haiku-20241022"
-GEMINI_MINI_MODEL_NAME = "gemini-2.5-flash-preview-04-17"   # 05-20 is broken for now for tool calling. Update when fixed.
+ANTHROPIC_MINI_MODEL_NAME = "claude-opus-4-20250514" # claude-3-7-sonnet-20250219
+GEMINI_MINI_MODEL_NAME = "gemini-2.5-flash-preview-05-20"   # 05-20 is broken for now for tool calling. Update when fixed.
 OPENAI_MINI_MODEL_NAME = "o4-mini"
 MINI_MODEL: Literal["CLAUDE", "GEMINI", "OPENAI"] = "GEMINI"
 
